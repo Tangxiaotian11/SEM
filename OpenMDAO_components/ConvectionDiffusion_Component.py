34b17fa70a136cfc44edc32c853534c2a90300c6
SEhrm <simon@ehrmanntraut.de>
2021-10-04 01:35:25 +0200

    change of basis moved in implicit component

--- OpenMDAO_components/ConvectionDiffusion_Component.py
+++ OpenMDAO_components/ConvectionDiffusion_Component.py
@@ -5,41 +5,57 @@ import openmdao.api as om
 class ConvectionDiffusion_Component(om.ImplicitComponent):
     def initialize(self):
         # declare parameters
-        self.options.declare('solver', desc='solver object')
+        self.options.declare('solver_CD', desc='convection-diffusion solver object')
+        self.options.declare('solver_NS', desc='NAVIER-STOKES solver object')
 
     def setup(self):
-        self.cd = self.options['solver']
+        self.cd = self.options['solver_CD']
+        self.ns = self.options['solver_NS']
 
         # declare variables
-        self.add_output('T', val=np.zeros(self.cd.N), desc='T as global vector')
-        self.add_input('u', val=np.zeros(self.cd.N), desc='u as global vector')
-        self.add_input('v', val=np.zeros(self.cd.N), desc='v as global vector')
+        self.add_output('T_cd', val=np.zeros(self.cd.N), desc='T as CD global vector')
+        self.add_input('u_ns', val=np.zeros(self.ns.N), desc='u as NS global vector')
+        self.add_input('v_ns', val=np.zeros(self.ns.N), desc='v as NS global vector')
 
         self.iter_count_solve = 0  # num of get_update calls
 
+    def change_inputs(self, u_ns, v_ns):  # TODO types
+        """
+        changes the basis of u and v from NS to CD; this is a linear map
+        :param u_ns: u as NS global vector
+        :param v_ns: v as NS global vector
+        :return: (u as CD global vector, v as CD global vector)
+        """
+        # Note that, get_interpol requires a mesh grid but get_vector passes global vectors
+        shape = (2, self.ns._P*self.ns._N_ex+1, self.ns._P*self.ns._N_ey+1)  # shape of mesh grid
+        u_call = lambda x, y: self.ns._get_interpol(u_ns, np.reshape((x, y), shape)).flatten()  # mesh grid, as required
+        v_call = lambda x, y: self.ns._get_interpol(v_ns, np.reshape((x, y), shape)).flatten()
+        u_cd = self.cd._get_vector(f_func=u_call)
+        v_cd = self.cd._get_vector(f_func=v_call)
+        # u_cd, v_cd = u_ns, v_ns
+        return u_cd, v_cd
+
     def apply_nonlinear(self, inputs, outputs, residuals, **kwargs):
-        residuals['T'] = self.cd._get_residuals(outputs['T'], inputs['u'], inputs['v'])
+        residuals['T_cd'] = self.cd._get_residuals(outputs['T_cd'], *self.change_inputs(inputs['u_ns'], inputs['v_ns']))
 
     def linearize(self, inputs, outputs, partials, **kwargs):
-        self.cd._calc_jacobians(outputs['T'])
+        self.cd._calc_jacobians(outputs['T_cd'])
 
     def apply_linear(self, inputs, outputs, d_inputs, d_outputs, d_residuals, mode):
         if mode != 'fwd':
             raise ValueError('only forward mode implemented')
 
-        dT = d_outputs['T'] if 'T' in d_outputs else np.zeros(self.cd.N)
-        du = d_inputs['u'] if 'u' in d_inputs else np.zeros(self.cd.N)
-        dv = d_inputs['v'] if 'v' in d_inputs else np.zeros(self.cd.N)
-        d_residuals['T'] = self.cd._get_dresiduals(dT, du, dv)
+        dT = d_outputs['T_cd'] if 'T_cd' in d_outputs else np.zeros(self.cd.N)
+        d_residuals['T_cd'] = self.cd._get_dresiduals(dT, *self.change_inputs(d_inputs['u_ns'], d_inputs['v_ns']))
 
     def solve_linear(self, d_outputs, d_residuals, mode):
         if mode != 'fwd':
             raise ValueError('only forward mode implemented')
 
-        d_outputs['T'] = self.cd._get_update(d_residuals['T'], dT0=d_outputs['T'])
+        d_outputs['T_cd'] = self.cd._get_update(d_residuals['T_cd'], dT0=d_outputs['T_cd'])
 
         self.iter_count_solve += 1
 
     def solve_nonlinear(self, inputs, outputs):
-        outputs['T'] = self.cd._get_solution(inputs['u'], inputs['v'], outputs['T'])
+        outputs['T_cd'] = self.cd._get_solution(*self.change_inputs(inputs['u_ns'], inputs['v_ns']), T0=outputs['T_cd'])
         self.iter_count_solve += 1  # problem is linear so only one get_update call
--- OpenMDAO_components/NavierStokes_Component.py
+++ OpenMDAO_components/NavierStokes_Component.py
@@ -5,47 +5,62 @@ import openmdao.api as om
 class NavierStokes_Component(om.ImplicitComponent):
     def initialize(self):
         # declare parameters
-        self.options.declare('solver', desc='solver object')
+        self.options.declare('solver_NS', desc='NAVIER-STOKES solver object')
+        self.options.declare('solver_CD', desc='convection-diffusion solver object')
 
     def setup(self):
-        self.ns = self.options['solver']
+        self.ns = self.options['solver_NS']
+        self.cd = self.options['solver_CD']
 
         # declare variables
-        self.add_input('T', val=np.zeros(self.ns.N), desc='T as global vector')
-        self.add_output('u', val=np.zeros(self.ns.N), desc='u as global vector')
-        self.add_output('v', val=np.zeros(self.ns.N), desc='v as global vector')
-        self.add_output('p', val=np.zeros(self.ns.N), desc='p as global vector')
+        self.add_input('T_cd', val=np.zeros(self.ns.N), desc='T as CD global vector')
+        self.add_output('u_ns', val=np.zeros(self.ns.N), desc='u as NS global vector')
+        self.add_output('v_ns', val=np.zeros(self.ns.N), desc='v as NS global vector')
+        self.add_output('p_ns', val=np.zeros(self.ns.N), desc='p as NS global vector')
 
         self.iter_count_solve = 0  # num of get_update calls
+        
+    def change_inputs(self, T_cd):  # TODO types
+        """
+        changes the basis of T from CD to NS; this is a linear map
+        :param T_cd: T as CD global vector
+        :return: T as NS global vector
+        """
+        # Note that, get_interpol requires a mesh grid but get_vector passes global vectors
+        shape = (2, self.cd._P*self.cd._N_ex+1, self.cd._P*self.cd._N_ey+1)  # shape of mesh grid
+        T_call = lambda x, y: self.ns._get_interpol(T_cd, np.reshape((x, y), shape)).flatten()  # mesh grid, as required
+        T_ns = self.cd._get_vector(f_func=T_call)
+        # T_ns = T_cd
+        return T_ns
 
     def apply_nonlinear(self, inputs, outputs, residuals, **kwargs):
-        residuals['u'], residuals['v'], residuals['p'] = \
-            self.ns._get_residuals(outputs['u'], outputs['v'], outputs['p'], inputs['T'])
+        residuals['u_ns'], residuals['v_ns'], residuals['p_ns'] = \
+            self.ns._get_residuals(outputs['u_ns'], outputs['v_ns'], outputs['p_ns'], self.change_inputs(inputs['T_cd']))
 
     def linearize(self, inputs, outputs, partials, **kwargs):
-        self.ns._calc_jacobians(outputs['u'], outputs['v'])
+        self.ns._calc_jacobians(outputs['u_ns'], outputs['v_ns'])
 
     def apply_linear(self, inputs, outputs, d_inputs, d_outputs, d_residuals, mode):
         if mode != 'fwd':
             raise ValueError('only forward mode implemented')
 
-        du = d_outputs['u'] if 'u' in d_outputs else np.zeros(self.ns.N)
-        dv = d_outputs['v'] if 'v' in d_outputs else np.zeros(self.ns.N)
-        dp = d_outputs['p'] if 'p' in d_outputs else np.zeros(self.ns.N)
-        dT = d_inputs['T'] if 'T' in d_inputs else np.zeros(self.ns.N)
-        d_residuals['u'], d_residuals['v'], d_residuals['p'] = self.ns._get_dresiduals(du, dv, dp, dT)
+        du = d_outputs['u_ns'] if 'u_ns' in d_outputs else np.zeros(self.ns.N)
+        dv = d_outputs['v_ns'] if 'v_ns' in d_outputs else np.zeros(self.ns.N)
+        dp = d_outputs['p_ns'] if 'p_ns' in d_outputs else np.zeros(self.ns.N)
+        d_residuals['u_ns'], d_residuals['v_ns'], d_residuals['p_ns'] = \
+            self.ns._get_dresiduals(du, dv, dp, self.change_inputs(d_inputs['T_cd']))
 
     def solve_linear(self, d_outputs, d_residuals, mode):
         if mode != 'fwd':
             raise ValueError('only forward mode implemented')
 
-        d_outputs['u'], d_outputs['v'], d_outputs['p'] = \
-            self.ns._get_update(d_residuals['u'], d_residuals['v'], d_residuals['p'],
-                                d_outputs['u'], d_outputs['v'], d_outputs['p'])
+        d_outputs['u_ns'], d_outputs['v_ns'], d_outputs['p_ns'] = \
+            self.ns._get_update(d_residuals['u_ns'], d_residuals['v_ns'], d_residuals['p_ns'],
+                                du0=d_outputs['u_ns'], dv0=d_outputs['v_ns'], dp0=d_outputs['p_ns'])
 
         self.iter_count_solve += 1
 
     def solve_nonlinear(self, inputs, outputs):
-        sol = self.ns._get_solution(inputs['T'], outputs['u'], outputs['v'], outputs['p'])
-        outputs['u'], outputs['v'], outputs['p'] = sol
+        outputs['u_ns'], outputs['v_ns'], outputs['p_ns']\
+            = self.ns._get_solution(self.change_inputs(inputs['T_cd']), u0=outputs['u_ns'], v0=outputs['v_ns'], p0=outputs['p_ns'])
         self.iter_count_solve += self.ns._k  # get num of get_update calls as num of inner NEWTON iterations
