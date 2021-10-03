919678ec1e72b69f2d6deac73349b178d625d18b
SEhrm <simon@ehrmanntraut.de>
2021-10-03 22:58:41 +0200

    fixes; updated Boussinesq_run.py

--- OpenMDAO_components/Boussinesq_OpenMDAO.py
+++ OpenMDAO_components/Boussinesq_OpenMDAO.py
@@ -40,32 +40,28 @@ mtol_internal = 1e-13  # tolerance on root mean square residual for internal sol
 mtol_gmres = 1e-10  # tolerance on root mean square residual for GMRES
 mtol_nonlin = 1e-8  # tolerance on root mean square residual for NEWTON
 
-N = (N_ex*P+1)*(N_ey*P+1)
-DOF = 4*N
+N = (N_ex*P+1)*(N_ey*P+1)  # grid points per variable
+DOF = 7*N  # T,u,v,p,T_int,u_int,v_int
 atol_gmres = mtol_gmres * np.sqrt(DOF)
 atol_nonlin = mtol_nonlin * np.sqrt(DOF)
 
 # initialize backend solvers
-if rank == 0:
-    cd = ConvectionDiffusionSolver(L_x=L_x, L_y=L_y, Pe=Re*Pr,
-                                   P=P, N_ex=N_ex, N_ey=N_ey,
-                                   T_W=0.5, T_E=-0.5,
-                                   mtol=mtol_internal)
-    ns = None
-if rank == 1:
-    ns = NavierStokesSolver(L_x=L_x, L_y=L_y, Re=Re, Gr=Ra/Pr,
-                            P=P, N_ex=N_ex, N_ey=N_ey,
-                            mtol=mtol_internal, mtol_newton=mtol_internal, iprint=['NEWTON_suc'])
-    cd = None
+cd = ConvectionDiffusionSolver(L_x=L_x, L_y=L_y, Pe=Re*Pr,
+                               P=P, N_ex=N_ex, N_ey=N_ey,
+                               T_W=0.5, T_E=-0.5,
+                               mtol=mtol_internal)
+ns = NavierStokesSolver(L_x=L_x, L_y=L_y, Re=Re, Gr=Ra/Pr,
+                        P=P, N_ex=N_ex, N_ey=N_ey,
+                        mtol=mtol_internal, mtol_newton=mtol_internal, iprint=['NEWTON_suc'])
 
 # initialize OpenMDAO solver
 prob = om.Problem()
 model = prob.model
 parallel = model.add_subsystem('parallel', om.ParallelGroup())
-parallel.add_subsystem('ConvectionDiffusion', ConvectionDiffusion_Component(solver=cd))
-parallel.add_subsystem('CD2NS', CD2NS_Component(solver_from=cd, solver_to=ns))
-parallel.add_subsystem('NavierStokes', NavierStokes_Component(solver=ns))
-parallel.add_subsystem('NS2CD', NS2CD_Component(solver_from=ns, solver_to=cd))
+parallel.add_subsystem('NavierStokes', NavierStokes_Component(solver=ns))  # rank 1
+parallel.add_subsystem('ConvectionDiffusion', ConvectionDiffusion_Component(solver=cd))  # rank 0
+parallel.add_subsystem('NS2CD', NS2CD_Component(solver_from=ns, solver_to=cd))  # rank 1
+parallel.add_subsystem('CD2NS', CD2NS_Component(solver_from=cd, solver_to=ns))  # rank 0
 parallel.connect('ConvectionDiffusion.T', 'CD2NS.T')
 parallel.connect('CD2NS.T_int', 'NavierStokes.T')
 parallel.connect('NavierStokes.u', 'NS2CD.u')
@@ -75,33 +71,15 @@ parallel.connect('NS2CD.v_int', 'ConvectionDiffusion.v')
 
 # - NEWTON
 parallel.nonlinear_solver = om.NewtonSolver(iprint=2, solve_subsystems=True, max_sub_solves=0, maxiter=1000, atol=atol_nonlin, rtol=0)
-parallel.nonlinear_solver.linesearch = om.ArmijoGoldsteinLS(iprint=2, maxiter=8, rho=0.8, c=0.2)
+# parallel.nonlinear_solver.linesearch = om.ArmijoGoldsteinLS(iprint=2, maxiter=8, rho=0.8, c=0.2)
 # --- JACOBI preconditioned NEWTON-KRYLOV
 parallel.linear_solver = om.PETScKrylov(iprint=2, atol=atol_gmres, rtol=0, restart=20, ksp_type='gmres', precon_side='left')
-parallel.linear_solver.precon = om.LinearBlockJac(iprint=-1, rtol=0, atol=0, maxiter=1)  # require change in linear_block_jac.py
+parallel.linear_solver.precon = om.LinearBlockJac(iprint=-1, rtol=0, atol=0, maxiter=1)  # requires change in linear_block_jac.py
 # --- NEWTON-JACOBI
-#parallel.linear_solver = om.LinearBlockJac(iprint=-1, rtol=0, atol=0, maxiter=1)  # require change in linear_block_jac.py
+#parallel.linear_solver = om.LinearBlockJac(iprint=-1, rtol=0, atol=0, maxiter=1)  # requires change in linear_block_jac.py
 # - Nonlinear GAUSS-SEIDEL
-#model.nonlinear_solver = om.NonlinearBlockGS(iprint=2, use_apply_nonlinear=True, maxiter=1000, atol=atol_nonlin, rtol=0)  # require change in nonlinear_block_gs.py
+#parallel.nonlinear_solver = om.NonlinearBlockGS(iprint=2, use_apply_nonlinear=True, maxiter=1000, atol=atol_nonlin, rtol=0)  # requires change in nonlinear_block_gs.py
 prob.setup()
-
-# preprocessing
-if rank == 0:
-    # grid
-    points = SEM.global_nodes(P, N_ex, N_ey, L_x/N_ex, L_y/N_ey)
-    # initial guess
-    T = np.zeros(N)
-    T[np.isclose(points[0], 0)] = 0.5
-    T[np.isclose(points[0], L_x)] = -0.5
-    # hand over guess
-    prob['parallel.ConvectionDiffusion.T'] = T
-if rank == 1:
-    # initial guess
-    u = np.zeros(N)
-    v = np.zeros(N)
-    # hand over guess
-    prob['parallel.NavierStokes.u'] = u
-    prob['parallel.NavierStokes.v'] = v
 
 # solve
 prob.run_model()
--- study/Boussinesq_run.py
+++ study/Boussinesq_run.py
@@ -7,6 +7,8 @@ from Examples.ConvectionDiffusionSolver import ConvectionDiffusionSolver
 from Examples.NavierStokesSolver import NavierStokesSolver
 from OpenMDAO_components.ConvectionDiffusion_Component import ConvectionDiffusion_Component
 from OpenMDAO_components.NavierStokes_Component import NavierStokes_Component
+from OpenMDAO_components.CD2NS_Component import CD2NS_Component
+from OpenMDAO_components.NS2CD_Component import NS2CD_Component
 import openmdao.api as om
 from mpi4py import MPI
 rank = MPI.COMM_WORLD.Get_rank()
@@ -15,11 +17,11 @@ rank = MPI.COMM_WORLD.Get_rank()
 class Logger(object):
     def __init__(self, file):
         self.terminal = sys.__stdout__
-        self.log = open(file, "a")
+        self.log = open(file, "w")
 
     def write(self, message):
-        self.terminal.write(message)
         self.log.write(message)
+        self.terminal.write(message)
 
     def flush(self): pass
 
@@ -27,14 +29,14 @@ class Logger(object):
 def run(log=False, save=True, mode='JNK', backend='PETSc',
         L_x=1., L_y=1., Re=1.e2, Ra=1.e3, Pr=0.71,
         P=4, Ne=8,
-        mtol_nonlin=1e-8, AGi=8, AGr=0.8, AGc=0.2,
+        mtol_nonlin=1e-8, AGi=0, AGr=0.0, AGc=0.0,
         mtol_gmres=1e-10, restart=20,
         mtol_internal=1e-13):
 
         N_ex = Ne
         N_ey = Ne
-        N = (N_ex*P+1)*(N_ey*P+1)
-        DOF = 4*N
+        N = (N_ex*P+1)*(N_ey*P+1)  # grid points per variable
+        DOF = 7*N  # T,u,v,p,T_int,u_int,v_int
         atol_gmres = mtol_gmres*np.sqrt(DOF)
         atol_nonlin = mtol_nonlin*np.sqrt(DOF)
         if mode == 'GS':
@@ -45,35 +47,36 @@ def run(log=False, save=True, mode='JNK', backend='PETSc',
             print(title)
 
         # initialize backend solvers
-        if rank == 0:
-            cd = ConvectionDiffusionSolver(L_x=L_x, L_y=L_y, Pe=Re*Pr,
-                                           P=P, N_ex=N_ex, N_ey=N_ey,
-                                           T_W=0.5, T_E=-0.5,
-                                           mtol=mtol_internal)
-            ns = None
-        if rank == 1:
-            ns = NavierStokesSolver(L_x=L_x, L_y=L_y, Re=Re, Gr=Ra/Pr,
-                                    P=P, N_ex=N_ex, N_ey=N_ey,
-                                    mtol=mtol_internal, mtol_newton=mtol_internal, iprint=['NEWTON_suc'])
-            cd = None
+        cd = ConvectionDiffusionSolver(L_x=L_x, L_y=L_y, Pe=Re*Pr,
+                                       P=P, N_ex=N_ex, N_ey=N_ey,
+                                       T_W=0.5, T_E=-0.5,
+                                       mtol=mtol_internal)
+        ns = NavierStokesSolver(L_x=L_x, L_y=L_y, Re=Re, Gr=Ra/Pr,
+                                P=P, N_ex=N_ex, N_ey=N_ey,
+                                mtol=mtol_internal, mtol_newton=mtol_internal, iprint=['NEWTON_suc'])
 
         # initialize OpenMDAO solver
         prob = om.Problem()
         model = prob.model
         parallel = model.add_subsystem('parallel', om.ParallelGroup())
-        parallel.add_subsystem('ConvectionDiffusion', ConvectionDiffusion_Component(solver=cd))
-        parallel.add_subsystem('NavierStokes', NavierStokes_Component(solver=ns))
-        parallel.connect('ConvectionDiffusion.T', 'NavierStokes.T')
-        parallel.connect('NavierStokes.u', 'ConvectionDiffusion.u')
-        parallel.connect('NavierStokes.v', 'ConvectionDiffusion.v')
+        parallel.add_subsystem('NavierStokes', NavierStokes_Component(solver=ns))  # rank 1
+        parallel.add_subsystem('ConvectionDiffusion', ConvectionDiffusion_Component(solver=cd))  # rank 0
+        parallel.add_subsystem('NS2CD', NS2CD_Component(solver_from=ns, solver_to=cd))  # rank 1
+        parallel.add_subsystem('CD2NS', CD2NS_Component(solver_from=cd, solver_to=ns))  # rank 0
+        parallel.connect('ConvectionDiffusion.T', 'CD2NS.T')
+        parallel.connect('CD2NS.T_int', 'NavierStokes.T')
+        parallel.connect('NavierStokes.u', 'NS2CD.u')
+        parallel.connect('NS2CD.u_int', 'ConvectionDiffusion.u')
+        parallel.connect('NavierStokes.v', 'NS2CD.v')
+        parallel.connect('NS2CD.v_int', 'ConvectionDiffusion.v')
 
         if mode == 'GS':
-            parallel.nonlinear_solver = om.NonlinearBlockGS(iprint=2, use_apply_nonlinear=True, maxiter=1000, atol=atol_nonlin, rtol=0)    # require change in nonlinear_block_gs.py
+            parallel.nonlinear_solver = om.NonlinearBlockGS(iprint=2, use_apply_nonlinear=True, maxiter=1000, atol=atol_nonlin, rtol=0)    # requires change in nonlinear_block_gs.py
         else:
             parallel.nonlinear_solver = om.NewtonSolver(iprint=2, solve_subsystems=True, max_sub_solves=0, maxiter=1000, atol=atol_nonlin, rtol=0)
             parallel.nonlinear_solver.linesearch = om.ArmijoGoldsteinLS(iprint=2, maxiter=AGi, rho=AGr, c=AGc)
             if mode == 'NJ':
-                parallel.linear_solver = om.LinearBlockJac(iprint=-1, rtol=0, atol=0, maxiter=1)  # require change in linear_block_jac.py
+                parallel.linear_solver = om.LinearBlockJac(iprint=-1, rtol=0, atol=0, maxiter=1)  # requires change in linear_block_jac.py
             elif mode == 'JNK':
                 if backend == 'SciPy':
                     parallel.linear_solver = om.ScipyKrylov(iprint=2, atol=atol_gmres, rtol=0, restart=restart, maxiter=5000)
@@ -82,7 +85,7 @@ def run(log=False, save=True, mode='JNK', backend='PETSc',
                                                             ksp_type='gmres', precon_side='left')
                 else:
                     raise ValueError('Unknown backend')
-                parallel.linear_solver.precon = om.LinearBlockJac(iprint=-1, rtol=0, atol=0, maxiter=1)  # require change in linear_block_jac.py
+                parallel.linear_solver.precon = om.LinearBlockJac(iprint=-1, rtol=0, atol=0, maxiter=1)  # requires change in linear_block_jac.py
             else:
                 raise ValueError('Unknown method')
         prob.setup()
@@ -164,6 +167,8 @@ if __name__ == "__main__":
             mode = sys.argv[i+1]
         if arg == '-log':
             log = bool(sys.argv[i+1])
+        if arg == '-save':
+            save = bool(sys.argv[i+1])
         if arg == '-backend':
             backend = sys.argv[i+1]
 
